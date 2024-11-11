e682c28f1b5ec55c3180e8bdf1edea68fa01c078
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-11-11 16:52:00 +0400

    harmony-shop first page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
-const body = document.querySelector("body");
 let html = "";
 
-const home = document.querySelector(".item-shop__container");
+const home = document.querySelector(".top-selling__container");
 
-fetch("https://dummyjson.com/products?limit=42")
+const data = fetch("https://dummyjson.com/products?limit=42")
   .then((res) => res.json())
   .then((json) => {
-    html += ``;
     console.log(json);
+    return json;
   });
+
+// const fn = async () => {
+//   const fetchedData = await data;
+//   for (let i = 0; i < 42; i++) {
+//     const element = fetchedData[i];
+//     html += `
+//     <div class="top-selling--card">
+//               <img src="${fetchedData.products[i].thumbnail}" alt="shop-item" />
+//               <div>
+//                 <h3>Mascara mascara</h3>
+//                 <p>Rating</p>
+//                 <strong>Price:</strong>
+//               </div>
+//               <button class="add-cart-btn">
+//                 <img src="./assets/images/icons/add-to-cart.png" alt="" />Add to
+//                 cart
+//               </button>
+//             </div>
+//     `;
+//   }
+//   home.innerHTML = html;
+// };
+
+// fn();
